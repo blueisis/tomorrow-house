d0d84886beec13600fc7774c07a851e4a0df9748
blueisis <pureun96@naver.com>
2022-07-26 13:27:27 +0900

    🚧 Implement toggling bookmark button

--- js/order-cta.js
+++ js/order-cta.js
@@ -1,23 +1,49 @@
-const orderCta = document.querySelector('.order-cta');
+const orderCta = document.querySelector('.order-cta')
 
-const [orderCtaBookmarkButton, orderCtaBuyButton] = orderCta.children;
+const [orderCtaBookmarkButton, orderCtaBuyButton] = orderCta.children
 
-console.log(orderCtaBookmarkButton, orderCtaBuyButton);
+console.log(orderCtaBookmarkButton, orderCtaBuyButton)
 
-const orderModal = document.querySelector('.order-form-modal');
-const orderModalOverlay = document.querySelector('.overlay');
+const orderModal = document.querySelector('.order-form-modal')
+const orderModalOverlay = document.querySelector('.overlay')
 
 function openOrderModal() {
-  orderModal.classList.add('is-open');
-  orderModalOverlay.classList.add('is-active');
+  orderModal.classList.add('is-open')
+  orderModalOverlay.classList.add('is-active')
 }
 
-orderCtaBuyButton.addEventListener('click', openOrderModal);
+orderCtaBuyButton.addEventListener('click', openOrderModal)
 
 function closeOrderModal() {
-  orderModal.classList.remove('is-open');
-  orderModalOverlay.classList.remove('is-active');
+  orderModal.classList.remove('is-open')
+  orderModalOverlay.classList.remove('is-active')
 }
 
-orderModalOverlay.addEventListener('click', closeOrderModal);
+orderModalOverlay.addEventListener('click', closeOrderModal)
+
+function toggleOrderCtaBookmark() {
+  const [icon, countSpan] = this.children
+  // const count = countSpan.innerHTML.replaceAll(',', '')
+  const count = Number(countSpan.innerHTML.replaceAll(',', ''))
+
+  let newCount = count
+
+  if (this.classList.contains('is-active')) {
+    // 앞으로 비활성화 
+    icon.classList.add('ic-bookmark')
+    icon.classList.remove('ic-bookmark-filled')
+    newCount = newCount - 1
+  } else {
+    // 앞으로 활성화 
+    icon.classList.add('ic-bookmark-filled')
+    icon.classList.remove('ic-bookmark')
+    newCount = newCount + 1
+  }
+
+  countSpan.innerHTML = newCount.toLocaleString()
+
+  this.classList.toggle('is-active')
+}
+
+orderCtaBookmarkButton.addEventListener('click', toggleOrderCtaBookmark)
 
